67fdb6073707bdb819c7146dfe4c2480d4b2e9fb
Robbanfriman <robert.friman@iths.se>
2023-11-07 17:07:10 +0100

    enemies and character

--- Dice.java
+++ Dice.java
@@ -1,3 +1,13 @@
+import java.util.Random;
+
 public class Dice {
-    
+
+
+
+    private Random random = new Random();
+
+    public int roll() {
+        return random.nextInt(6) + 1; 
+    }
 }
+
